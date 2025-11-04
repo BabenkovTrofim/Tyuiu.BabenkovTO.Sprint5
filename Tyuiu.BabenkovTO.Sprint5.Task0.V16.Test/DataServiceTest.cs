1e5d084a170bcdccdba4865a6aaba026e3472517
BabenkovTrofim <babenkovt@bk.ru>
2025-11-04 14:11:15 +0500

    Add project files.

---
+++ Tyuiu.BabenkovTO.Sprint5.Task0.V16.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BabenkovTO.Sprint5.Task0.V16.Lib;
+namespace Tyuiu.BabenkovTO.Sprint5.Task0.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string[] path = { @"C:\Users", "Trofim", "source", "repos", "Tyuiu.BabenkovTO.Sprint5", "Tyuiu.BabenkovTO.Sprint5.Task0.V16", "bin", "Debug", "net8.0", "OutPutFileTask0.txt" };
+            string fullPath = Path.Combine(path);
+            FileInfo fileInfo = new FileInfo(fullPath);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.BabenkovTO.Sprint5.Task0.V16/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.BabenkovTO.Sprint5.Task0.V16.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #5 | Выполнил: Бабенков Т. О. | ИИПб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #16                                                             *");
+        Console.WriteLine("* Выполнил: Бабенков Трофим Олегович | ИИПб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить в  *");
+        Console.WriteLine("* текстовый файл OutPutFileTask0.txt и вывести на консоль. Округлить до   *");
+        Console.WriteLine("* трёх знаков после запятой.                                              *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int x = 3;
+        Console.WriteLine($"Значение Х = {x}");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        DataService ds = new DataService();
+        string res = ds.SaveToFileTextData(x);
+        Console.WriteLine($"Файл: {res}");
+        Console.WriteLine("Создан!");
+    }
+}
