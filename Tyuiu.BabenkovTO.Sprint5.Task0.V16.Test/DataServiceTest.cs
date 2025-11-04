dcf4fb22fd91e4f219bcd78d01f1f93931b23d67
BabenkovTrofim <babenkovt@bk.ru>
2025-11-04 14:30:16 +0500

    Task0

--- Tyuiu.BabenkovTO.Sprint5.Task0.V16.Test/DataServiceTest.cs
+++ Tyuiu.BabenkovTO.Sprint5.Task0.V16.Test/DataServiceTest.cs
@@ -8,9 +8,8 @@ namespace Tyuiu.BabenkovTO.Sprint5.Task0.V16.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string[] path = { @"C:\Users", "Trofim", "source", "repos", "Tyuiu.BabenkovTO.Sprint5", "Tyuiu.BabenkovTO.Sprint5.Task0.V16", "bin", "Debug", "net8.0", "OutPutFileTask0.txt" };
-            string fullPath = Path.Combine(path);
-            FileInfo fileInfo = new FileInfo(fullPath);
+            string path = @"C:\Users\Trofim\source\repos\Tyuiu.BabenkovTO.Sprint5\Tyuiu.BabenkovTO.Sprint5.Task0.V16\bin\Debug\net8.0\OutPutFileTask0.txt";
+            FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
             bool wait = true;
             Assert.AreEqual(wait, fileExists);
