3d6d54b7e4edc6234b662560f3874cdfc38353a4
BabenkovTrofim <babenkovt@bk.ru>
2025-11-07 18:25:37 +0500

    Task3

---
+++ Tyuiu.BabenkovTO.Sprint5.Task3.V11.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Tyuiu.BabenkovTO.Sprint5.Task3.V11.Lib;
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
+namespace Tyuiu.BabenkovTO.Sprint5.Task3.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()           
+        {
+            string path = Path.Combine(new string[] { "C:", "Users", "Trofim", "AppData", "Local", "Temp", "OutPutFileTask3.bin" });
+            FileInfo file = new FileInfo(path);
+            Assert.AreEqual(true, file.Exists);
+        }
+    }
+}
---
+++ Tyuiu.BabenkovTO.Sprint5.Task3.V11/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.BabenkovTO.Sprint5.Task3.V11.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #5 | Выполнил: Бабенков Т. О. | ИИПб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Класс File. Потоковый метод записи данных в бинарный файл         *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #11                                                             *");
+        Console.WriteLine("* Выполнил: Бабенков Трофим Олегович | ИИПб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить    *");
+        Console.WriteLine("* в бинарный файл OutPutFileTask3.bin и вывести на консоль. Округлить до  *");
+        Console.WriteLine("* трёх знаков после запятой.                                              *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int x = 3;
+        Console.WriteLine($"Значение X = {x}");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        DataService ds = new DataService();
+        string res = ds.SaveToFileTextData(x);
+        Console.WriteLine($"Файл: {res} \n\r Создан");
+    }
+}
